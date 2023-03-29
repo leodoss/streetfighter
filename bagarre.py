fb669698043fe26a0ed4dcb387090d24f3b3d491
Leo <62615866+leodoss@users.noreply.github.com>
2023-03-29 14:48:36 +0200

    Update bagarre.py

--- bagarre.py
+++ bagarre.py
@@ -4,8 +4,10 @@ class Personnage(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
         self.posebaseversdroite = []
-        self.posebaseversdroite.append(pygame.image.load('images/posebaseversdroite1.png'))
-        self.posebaseversdroite.append(pygame.image.load('images/posebaseversdroite2.png'))
+        im1 = pygame.image.load('images/posebaseversdroite1.png')
+        im2 = pygame.image.load('images/posebaseversdroite2.png')
+        self.posebaseversdroite.append(pygame.transform.scale(im1, (im1.get_rect().width*2, im1.get_rect().height*2)))
+        self.posebaseversdroite.append(pygame.transform.scale(im2, (im2.get_rect().width*2, im2.get_rect().height*2)))
         
         self.curseur = 0
         self.image = self.posebaseversdroite[self.curseur]
@@ -18,7 +20,7 @@ class Personnage(pygame.sprite.Sprite):
         C'est une méthode de la classe mère Sprite qui ne fait rien de base
         et qu'on décide donc de surcharger
         '''
-        self.curseur += 0.05
+        self.curseur += 0.1
         if self.curseur >= len(self.posebaseversdroite):
             self.curseur = 0
         
@@ -35,7 +37,7 @@ background = pygame.image.load('images/terrain.jpg')
 
 # Importation des sprites
 group_personnages = pygame.sprite.Group()
-perso1 = Personnage(10, 800)
+perso1 = Personnage(10, 770)
 group_personnages.add(perso1)
 
 while running:
